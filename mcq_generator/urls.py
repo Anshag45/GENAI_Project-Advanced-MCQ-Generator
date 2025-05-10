b089479fd70ee00b6eac7eab77ae82bc614e220f
Ansh Agarwal <agarwalansh651@gmail.com>
2025-05-11 02:15:18 +0530

    Add files via upload

--- mcq_generator/urls.py
+++ mcq_generator/urls.py
@@ -1,24 +1,11 @@
-"""
-URL configuration for ques_generator project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('auth/', include('authapp.urls')),
-    path('admin/', admin.site.urls),
-    path('ques/', include("ques_maker_app.urls")),
+    path('index/', views.index, name='index'),
+    path('save_questions/', views.save_questions, name='save_questions'),
+    path('get_questions/', views.get_questions, name='get_questions'),
+    path('delete_getsingle_question/<qId>',
+         views.delete_getsingle_question, name='delete_getsingle_question'),
+    path('update_question/', views.update_question, name='update_question'),
 ]
